1eb20cc266bb104807db93ea22e8b31d1a86268d
robtomb <robtomb@outlook.com>
2017-08-09 14:29:10 +0800

    fibonacci series

--- fibonacci.js
+++ fibonacci.js
@@ -4,7 +4,7 @@ function fibonacciSeries(num) {
 		return 0;
 	if(num === 2)
 		return 1;
-	return fibonacciSeries(num - 1) + fibonacciSeries(num - 2);;
+	return fibonacciSeries(num - 1) + fibonacciSeries(num - 2);
 }
 
 console.log(fibonacciSeries(10));
