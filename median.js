4c2d14ab8a5ef6b15fca514e4ea2f179ed558e0c
robtomb <robtomb@outlook.com>
2017-08-13 14:14:54 +0800

    find median

--- median.js
+++ median.js
@@ -1,12 +1,7 @@
 
 function median(arr) {
 	arr = arr.sort( (x , y)=>{ return x - y ;})
-	let median = 0;
-	if( arr.length % 2 === 0 )
-		median = arr[arr.length / 2 - 1] / 2 + arr[arr.length / 2] / 2;
-	else
-		median = arr[parseInt(arr.length / 2)]
-	return median;
+	return arr.length % 2 === 0 ? arr[arr.length / 2 - 1] / 2 + arr[arr.length / 2] / 2 : arr[parseInt(arr.length / 2)];
 }
 
 console.log(median([3,2,1,4,5]));
