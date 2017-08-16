795e9234a52d72019bd5e3eee831c15222c7afbb
robtomb <robtomb@outlook.com>
2017-08-16 14:21:24 +0800

    find repete item

---
+++ repeteItem.js
@@ -0,0 +1,16 @@
+/*
+题目：找出数组 arr 中重复出现过的元素
+例如：arr = ['a', ‘ab’, ‘ed’, ‘da’, ‘a’, ‘ed’, ‘b’];输出：‘a’, ‘ed’
+ */
+
+function repeteItem(arr) {
+	let result = [];
+	arr.forEach( (element,index,array)=>{
+		if( arr.indexOf(element) !== arr.lastIndexOf(element) && !result.includes(element) )
+			result.push(element);
+	})
+	return result;
+}
+
+console.log(repeteItem(['a' , 'ab', 'ed', 'da', 'a', 'ed', 'b']));
+console.log(repeteItem(['a' , 'a', 'a', 'a', 'a']));
