e531cc82b986d2025c4c79ff22126a23d795f646
robtomb <robtomb@outlook.com>
2017-08-23 09:39:30 +0800

    console sort object

--- countSameItem.js
+++ countSameItem.js
@@ -6,6 +6,17 @@
 
  */
 
+function sortObj(obj) {
+	let keyArr = Object.keys(obj).sort( ( y , x )=> {return obj[x] - obj[y]} );
+	let result = {};
+	for (var i = 0; i < keyArr.length; i++) {
+		result[keyArr[i]] = obj[keyArr[i]];
+		if( obj[keyArr[i]] !== obj[keyArr[i+1]] )
+			break;
+	}
+	return result;
+}
+
 function countSameItem(str) {
 	let result = {};
 	for(item of str){
@@ -14,7 +25,8 @@ function countSameItem(str) {
 		else
 			result[item] = 1;
 	}
-	return result;
+
+	return sortObj(result);
 }
 
 console.log(countSameItem('aaaaabbdbdbb'));
