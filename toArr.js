84964277dfc936d0e906a2e8f52d710a8055adc0
robtomb <robtomb@outlook.com>
2017-08-24 14:15:13 +0800

    String padStart

--- toArr.js
+++ toArr.js
@@ -1,3 +1,11 @@
+function oneArr(arr) {
+	return arr.join('').replace(/,/g , '').split('').map( i => parseInt(i));
+}
+
+console.log(oneArr([1,2,[1,2]]));
+console.log(oneArr([1,2,[1,2],[3,4]]));
+console.log(oneArr([4,[3,[2,[1]]]]));
+
 /*
 给定一个数组，该数组中的每个元素要么是个数组，要么是整数。将其变成一个只包含整数的简单数组。
 给定 [1,2,[1,2]]，返回 [1,2,1,2]。
@@ -7,11 +15,4 @@
 
  */
 
-function oneArr(arr) {
-	return arr.join('').replace(/,/g , '').split('').map( i => parseInt(i));
-}
-
-console.log(oneArr([1,2,[1,2]]));
-console.log(oneArr([1,2,[1,2],[3,4]]));
-console.log(oneArr([4,[3,[2,[1]]]]));
 
