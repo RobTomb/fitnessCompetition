eea86b826a5ea5aca31ff139325eebdee3e056fe
robtomb <robtomb@outlook.com>
2017-08-25 15:19:07 +0800

    find and move another site

--- leftpad.js
+++ leftpad.js
@@ -1,3 +1,8 @@
+function leftpad(str , count , strA) {
+	return str.padStart(count - str.length ,strA);
+}
+
+console.log(leftpad('hello',20,'1'));
 /*
 实现一个left-pad函数
 leftpad功能，就是字符串前面拼指定字符到固定长度，比如
@@ -7,9 +12,3 @@ leftpad(‘hello’,20,‘1’)，就要返回’111111111111111hello’
 第二个：需要返回字符串的长度
 第三个：用来拼接的字符串
  */
-
-function leftpad(str , count , strA) {
-	return str.padStart(str.length + count,strA);
-}
-
-console.log(leftpad('hello',20,'1'));
